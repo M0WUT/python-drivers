f02fed6df098d4f622bc74648f23f30a43a37aff
M0WUT <m0wutradio@gmail.com>
2024-08-03 19:56:28 +0100

    update

--- src/m0wut_drivers/pi4.py
+++ src/m0wut_drivers/pi4.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 
 class RaspberryPi4:
@@ -11,10 +12,23 @@ class RaspberryPi4:
         )
         return float(temperature) / 1000
 
+    def read_memory_usage_percent(self) -> int:
+        """Returns total RAM in bytes"""
+        results = (
+            subprocess.check_output(["cat", "/proc/meminfo"]).strip().decode()
+        )
+        memstats = {}
+        results = [re.split(r"\s+", x[:-3]) for x in results.split("\n")]
+        for item in results:
+            key = item[0][:-1]
+            value = item[1]
+            memstats[key] = int(value)
+        return int(100 * (1 - memstats["MemAvailable"] / memstats["MemTotal"]))
+
 
 def main() -> None:
     x = RaspberryPi4()
-    print(x.read_cpu_temperature())
+    print(x.read_memory_usage_percent())
 
 
 if __name__ == "__main__":
